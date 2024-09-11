ced57adce7fc50d7a38af5450352ffa362280af6
dongchao <afkxwz@gmail.com>
2024-09-11 14:11:40 +0800

    添加MoveException异常类和模块注释

--- custom_exception.py
+++ custom_exception.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+自定义异常类
+"""
 class APIException(Exception):
     """自定义异常类，用于处理无法获取API的异常"""
     def __init__(self, message="无法获取API"):
@@ -8,5 +11,11 @@ class APIException(Exception):
 class CopyException(Exception):
     """自定义异常类，用于处理复制错误的异常"""
     def __init__(self, message="复制文件时发生错误"):
+        self.message = message
+        super().__init__(self.message)
+
+class MoveException(Exception):
+    """自定义异常类，用于处理移动错误的异常"""
+    def __init__(self, message="移动文件时发生错误"):
         self.message = message
         super().__init__(self.message)
